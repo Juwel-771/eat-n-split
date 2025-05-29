ac8535ed936ea832068643016a50f3295f73fc9a
Juwel-771 <juwelah99@gmail.com>
2025-05-29 21:45:46 +0600

    add form validate

--- src/component/FormAddFrined.jsx
+++ src/component/FormAddFrined.jsx
@@ -6,8 +6,23 @@ const FormAddFrined = () => {
     const [name, setName] = useState("");
     const [image, setImage] = useState("https://i.pravatar.cc/48");
 
-    function handleSubmit () {
+    function handleSubmit (e) {
+        e.preventDefault();
 
+        if(!name) return alert("Inert Name Please");
+        if(!image) return alert("Inert Image URL Please");
+
+        const id = crypto.randomUUID();
+        const newFriend = {
+            name, 
+            image: `${image}?=${id}`, 
+            balance: 0,
+            id
+        }
+
+        console.log(newFriend);
+        setName("");
+        setImage("https://i.pravatar.cc/48");
     }
 
     return (
