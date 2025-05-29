58d0416ebc6bc634e076f1318c1fe82aebfa439d
Juwel-771 <juwelah99@gmail.com>
2025-05-29 21:17:09 +0600

    add friend form functionalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 import Button from './component/Button';
 import FormAddFrined from './component/FormAddFrined';
@@ -6,13 +7,19 @@ import FriendList from './component/FriendList';
 
 
 function App() {
+  const [isAddForm, setIsAddForm] = useState(false);
+
+  const handleAddFriendForm = () => {
+    setIsAddForm(!isAddForm);
+    console.log(isAddForm);
+  }
 
   return (
     <div className='app'>
       <div className='sidebar'>
         <FriendList></FriendList>
-        <FormAddFrined></FormAddFrined>
-        <Button>Add Friend</Button>
+        {isAddForm ? <FormAddFrined/> : ""}
+       <Button onClick = {handleAddFriendForm}> {isAddForm ? "Close" : "Add Friend"} </Button>
       </div>
       <FormSplitBill></FormSplitBill>
     </div>
