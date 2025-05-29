b11e251340f94baf431fda799c34573d2f94a8c6
Juwel-771 <juwelah99@gmail.com>
2025-05-29 20:18:42 +0600

    project style created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import './App.css'
+import FriendList from './component/FriendList';
+
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
 function App() {
 
   return (
-    <>
-      <h1>Project SET</h1>
-    </>
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendList></FriendList>
+      </div>
+    </div>
   )
 }
 
---
+++ src/component/FriendList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FriendList = () => {
+    return (
+        <>
+            <ul>
+                List
+            </ul>
+        </>
+    );
+};
+
+export default FriendList;
