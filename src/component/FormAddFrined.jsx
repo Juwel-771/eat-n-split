429347ecb85fbe2461de716ade8f775c7a3bb92b
Juwel-771 <juwelah99@gmail.com>
2025-05-29 21:31:30 +0600

    add friend state d

--- src/component/FormAddFrined.jsx
+++ src/component/FormAddFrined.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
 const FormAddFrined = () => {
+
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+    function handleSubmit () {
+
+    }
+
     return (
-        <form className='form-add-friend'>
+        <form className='form-add-friend' onSubmit={handleSubmit}>
             <label>👭 Name: </label>
-            <input type="text" />
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
 
             <label>🖼 Image: </label>
-            <input type="text" />
+            <input type="text" value={image} />
 
             <Button>Add Friend</Button>
         </form>
